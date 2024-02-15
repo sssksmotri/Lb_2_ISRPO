01882d43878a23d8af3424b7946a3c9f76bdc695
kaoru <nsaidov530@gmail.com>
2024-02-15 13:25:22 +0300

    Добавьте файлы проекта.

---
+++ Lb_2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lb_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
